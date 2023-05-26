e2af2e5e5012fee2b15364d64afc2960867d8ca8
apineda03 <apineda03@ucatolica.edu.co>
2023-05-25 21:22:44 -0500

    profile update in profile.html login user exists validation called back for filtering by name-description

--- js/login.js
+++ js/login.js
@@ -1,98 +1,101 @@
 //USED CEDULA STYLE TO GET CSSTEXT FOR FORMER UPDATES ON FIELDS
-const css = document.getElementById("email")
-const previousStyle = css.style.cssText
+const css = document.getElementById("email");
+const previousStyle = css.style.cssText;
 
 //VALIDATION OF "EMAIL" "SOMETHING@ELSE.DOT"
 function emailValidation() {
-    const email = document.getElementById("email")
-    var input = document.getElementById("email").value
-    var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    var trimmedInput = input.trim()
+  const email = document.getElementById("email");
+  var input = document.getElementById("email").value;
+  var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var trimmedInput = input.trim();
 
-    if (input.match(pattern)) {
-        email.style.border = "2px solid #2FC11D"
-        setTimeout(() => {
-            email.style.cssText = previousStyle
-        }, 2000);
-        mostrarError(email, false);
-        return true
-    } else if (trimmedInput === "") {
-        email.style.cssText = previousStyle
-        mostrarError(email, true);
-        return false
-    } else {
-        email.style.border = "2px solid red"
-        mostrarError(email, true);
-        return false
-    }
+  if (input.match(pattern)) {
+    email.style.border = "2px solid #2FC11D";
+    setTimeout(() => {
+      email.style.cssText = previousStyle;
+    }, 2000);
+    mostrarError(email, false);
+    return true;
+  } else if (trimmedInput === "") {
+    email.style.cssText = previousStyle;
+    mostrarError(email, true);
+    return false;
+  } else {
+    email.style.border = "2px solid red";
+    mostrarError(email, true);
+    return false;
+  }
 }
 
-email.onkeyup = () => { emailValidation() }
+email.onkeyup = () => {
+  emailValidation();
+};
 
-var button = document.getElementById("loginBtn")
+var button = document.getElementById("loginBtn");
 
 function validateInput() {
+  var vpassword = document.getElementById("password").value;
+  var trimmedInput = vpassword.trim();
 
-    var vpassword = document.getElementById("password").value
-    var trimmedInput = vpassword.trim();
-
-    if (trimmedInput === "") {
-        return false
-    } else {
-        button.classList.remove("disabled")
-        return true
-    }
+  if (trimmedInput === "") {
+    return false;
+  } else {
+    button.classList.remove("disabled");
+    return true;
+  }
 }
 
-password.onkeyup = () => { validateInput() }
+password.onkeyup = () => {
+  validateInput();
+};
 
 document.getElementById("loginBtn").onclick = () => {
-    console.log("data")
-    let customer = {
-        "correo": document.getElementById("email").value,
-        "password": document.getElementById("password").value,
-    }
+  console.log("data");
+  let customer = {
+    correo: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
 
-    fetch("http://127.0.0.1:8080/api/customers/login", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(customer)
-    }).then(function (response) {
-        if (response.ok) {
-            // Procesar la respuesta de la API
-            response.json().then(function (data) {
-                setCookie("userBaihan", data.cedula, 7);
-                location.assign("./home.html")
-            });
+  fetch("http://127.0.0.1:8080/api/customers/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  }).then(function (response) {
+    if (response.ok) {
+      // Procesar la respuesta de la API
+      response.json().then(function (data) {
+        if (data.cedula == null) {
+          alert("No existe el usuario");
         } else {
-            // Manejar los errores de la API
-            //console.log("Error en la solicitud");
-            return false;
+          setCookie("userBaihan", data.cedula, 7);
+          location.assign("./home.html");
         }
-    });
-}
+      });
+    } else {
+      // Manejar los errores de la API
+      //console.log("Error en la solicitud");
+      return false;
+    }
+  });
+};
 
 function setCookie(nombre, valor, expiracion) {
-    var fechaExpiracion = new Date();
-    fechaExpiracion.setTime(fechaExpiracion.getTime() + expiracion * 24 * 60 * 60 * 1000);
-    var expira = "expires=" + fechaExpiracion.toUTCString();
-    document.cookie = nombre + "=" + valor + ";" + expira + ";path=/";
+  var fechaExpiracion = new Date();
+  fechaExpiracion.setTime(
+    fechaExpiracion.getTime() + expiracion * 24 * 60 * 60 * 1000
+  );
+  var expira = "expires=" + fechaExpiracion.toUTCString();
+  document.cookie = nombre + "=" + valor + ";" + expira + ";path=/";
 }
 
 function mostrarError(input, accion) {
-    let container = input.parentElement;
-    for (hijo of container.children) {
-        if (hijo.classList.contains("error-message")) {
-            if (accion)
-                hijo.classList.add("d-block");
-            else
-                hijo.classList.remove("d-block");
-        }
+  let container = input.parentElement;
+  for (hijo of container.children) {
+    if (hijo.classList.contains("error-message")) {
+      if (accion) hijo.classList.add("d-block");
+      else hijo.classList.remove("d-block");
     }
+  }
 }
-
-
-
-
