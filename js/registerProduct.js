35b5cf54b1180b439dda0f2d8dbaa05ae1a0dec2
Santiago <rsmoreno95@ucatolica.edu.co>
2023-05-30 15:42:22 -0500

    Servidor de imagenes en proceso de prueba

--- js/registerProduct.js
+++ js/registerProduct.js
@@ -3,93 +3,93 @@ const previousStyle = css.style.cssText;
 
 //VALIDATION OF EAN CODE
 function eanCodeValidation() {
-  const codigoean = document.getElementById("codigoean");
-  var input = document.getElementById("codigoean").value;
-  var pattern = /^[0-9]+$/;
+    const codigoean = document.getElementById("codigoean");
+    var input = document.getElementById("codigoean").value;
+    var pattern = /^[0-9]+$/;
 
-  codigoean.addEventListener("input", () => {
-    codigoean.value = codigoean.value.replace(/\D/g, "");
-  });
+    codigoean.addEventListener("input", () => {
+        codigoean.value = codigoean.value.replace(/\D/g, "");
+    });
 
-  if (input.match(pattern)) {
-    codigoean.style.border = "2px solid #2FC11D";
-    setTimeout(() => {
-      codigoean.style.cssText = previousStyle;
-    }, 2000);
-    mostrarError(codigoean, false);
-    return true;
-  } else {
-    codigoean.style.border = "2px solid red";
-    mostrarError(codigoean, true);
-  }
+    if (input.match(pattern)) {
+        codigoean.style.border = "2px solid #2FC11D";
+        setTimeout(() => {
+            codigoean.style.cssText = previousStyle;
+        }, 2000);
+        mostrarError(codigoean, false);
+        return true;
+    } else {
+        codigoean.style.border = "2px solid red";
+        mostrarError(codigoean, true);
+    }
 }
 
 codigoean.onkeyup = () => {
-  eanCodeValidation();
+    eanCodeValidation();
 };
 
 //VALIDATION OF "NOMBRE"
 function nameValidation() {
-  const nombre = document.getElementById("nombre");
-  var input = document.getElementById("nombre").value;
-  var pattern = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s'0-9]+$/;
+    const nombre = document.getElementById("nombre");
+    var input = document.getElementById("nombre").value;
+    var pattern = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s'0-9]+$/;
 
-  if (input.match(pattern)) {
-    nombre.style.border = "2px solid #2FC11D";
-    setTimeout(() => {
-      nombre.style.cssText = previousStyle;
-    }, 2000);
-    mostrarError(nombre, false);
-    return true;
-  } else {
-    nombre.style.border = "2px solid red";
-    mostrarError(nombre, true);
-  }
+    if (input.match(pattern)) {
+        nombre.style.border = "2px solid #2FC11D";
+        setTimeout(() => {
+            nombre.style.cssText = previousStyle;
+        }, 2000);
+        mostrarError(nombre, false);
+        return true;
+    } else {
+        nombre.style.border = "2px solid red";
+        mostrarError(nombre, true);
+    }
 }
 
 nombre.onkeyup = () => {
-  nameValidation();
+    nameValidation();
 };
 
 //VALIDATION OF "MARCA"
 function marcaValidation() {
-  const marca = document.getElementById("marca");
-  var input = document.getElementById("marca").value;
-  var pattern = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s'0-9]+$/;
+    const marca = document.getElementById("marca");
+    var input = document.getElementById("marca").value;
+    var pattern = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s'0-9]+$/;
 
-  if (input.match(pattern)) {
-    marca.style.border = "2px solid #2FC11D";
-    setTimeout(() => {
-      marca.style.cssText = previousStyle;
-    }, 2000);
-    mostrarError(marca, false);
-    return true;
-  } else {
-    marca.style.border = "2px solid red";
-    mostrarError(marca, true);
-  }
+    if (input.match(pattern)) {
+        marca.style.border = "2px solid #2FC11D";
+        setTimeout(() => {
+            marca.style.cssText = previousStyle;
+        }, 2000);
+        mostrarError(marca, false);
+        return true;
+    } else {
+        marca.style.border = "2px solid red";
+        mostrarError(marca, true);
+    }
 }
 
 marca.onkeyup = () => {
-  marcaValidation();
+    marcaValidation();
 };
 
 //VALIDATION OF "PRECIO"
 function precioValidation() {
-  const precio = document.getElementById("precio");
-  var input = document.getElementById("precio").value;
+    const precio = document.getElementById("precio");
+    var input = document.getElementById("precio").value;
 
-  if (input > 0) {
-    precio.style.border = "2px solid #2FC11D";
-    setTimeout(() => {
-        precio.style.cssText = previousStyle;
-      }, 2000);
-    mostrarError(precio, false);
-    return true;
-  } else {
-    precio.style.border = "2px solid red";
-    mostrarError(precio, true);
-  }
+    if (input > 0) {
+        precio.style.border = "2px solid #2FC11D";
+        setTimeout(() => {
+            precio.style.cssText = previousStyle;
+        }, 2000);
+        mostrarError(precio, false);
+        return true;
+    } else {
+        precio.style.border = "2px solid red";
+        mostrarError(precio, true);
+    }
 }
 
 precio.onkeyup = () => {
@@ -100,17 +100,17 @@ precio.onkeyup = () => {
 function cantidadValidation() {
     const cantidad = document.getElementById("cantidad");
     var input = document.getElementById("cantidad").value;
-  
+
     if (input > 0) {
-      cantidad.style.border = "2px solid #2FC11D";
-      setTimeout(() => {
-        cantidad.style.cssText = previousStyle;
-      }, 2000);
-      mostrarError(cantidad, false);
-      return true;
+        cantidad.style.border = "2px solid #2FC11D";
+        setTimeout(() => {
+            cantidad.style.cssText = previousStyle;
+        }, 2000);
+        mostrarError(cantidad, false);
+        return true;
     } else {
-      cantidad.style.border = "2px solid red";
-      mostrarError(cantidad, true);
+        cantidad.style.border = "2px solid red";
+        mostrarError(cantidad, true);
     }
 }
 
@@ -119,51 +119,51 @@ cantidad.onkeyup = () => {
 };
 
 function mostrarError(input, accion) {
-  let container = input.parentElement;
-  for (hijo of container.children) {
-    if (hijo.classList.contains("error-message")) {
-      if (accion) hijo.classList.add("d-block");
-      else hijo.classList.remove("d-block");
+    let container = input.parentElement;
+    for (hijo of container.children) {
+        if (hijo.classList.contains("error-message")) {
+            if (accion) hijo.classList.add("d-block");
+            else hijo.classList.remove("d-block");
+        }
     }
-  }
 }
 
 const button = document.getElementById("publicar");
 
 function validate() {
-  if (
-    eanCodeValidation() &&
-    nameValidation() &&
-    marcaValidation() &&
-    precioValidation() &&
-    cantidadValidation()
-  ) {
-    button.classList.remove("disabled");
-  }
+    if (
+        eanCodeValidation() &&
+        nameValidation() &&
+        marcaValidation() &&
+        precioValidation() &&
+        cantidadValidation()
+    ) {
+        button.classList.remove("disabled");
+    }
 }
 
 button.addEventListener("click", (event) => {
-  const eanCode = document.getElementById("codigoean").value;
-  const name = document.getElementById("nombre").value;
-  const description = document.getElementById("descripcion").value;
-  const brand = document.getElementById("marca").value;
-  const quantity = document.getElementById("cantidad").value;
-  const price = document.getElementById("precio").value;
-  const cedula = getCookie('userBaihan');
+    const eanCode = document.getElementById("codigoean").value;
+    const name = document.getElementById("nombre").value;
+    const description = document.getElementById("descripcion").value;
+    const brand = document.getElementById("marca").value;
+    const quantity = document.getElementById("cantidad").value;
+    const price = document.getElementById("precio").value;
+    const cedula = getCookie('userBaihan');
 
-  const product = {
-    eanCode: eanCode,
-    name: name,
-    description: description,
-    brand: brand,
-    quantity: quantity,
-    price: price,
-    customer: {cedula: cedula}
-  };
+    const product = {
+        eanCode: eanCode,
+        name: name,
+        description: description,
+        brand: brand,
+        quantity: quantity,
+        price: price,
+        customer: { cedula: cedula }
+    };
 
-  sendData("http://3.128.182.247/api/products/save", product);
+    sendData("http://3.128.182.247/api/products/save", product);
 
-  window.location.href = "home.html";
+    window.location.href = "home.html";
 });
 
 function getCookie(nombre) {
@@ -182,22 +182,56 @@ function getCookie(nombre) {
 }
 
 function sendData(url, object) {
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(object),
-  }).then(function (response) {
-    if (response.ok) {
-      // Procesar la respuesta de la API
-      response.json().then(function (data) {
-        return true;
-      });
-    } else {
-      // Manejar los errores de la API
-      //console.log("Error en la solicitud");
-      return false;
-    }
-  });
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(object),
+    }).then(function (response) {
+        if (response.ok) {
+            // Procesar la respuesta de la API
+            response.json().then(function (data) {
+                return true;
+            });
+        } else {
+            // Manejar los errores de la API
+            //console.log("Error en la solicitud");
+            return false;
+        }
+    });
 }
+
+AWS.config.update({
+    accessKeyId: 'TAKIA6MPTF7DYCGV2GPV5',
+    secretAccessKey: 'nYBhrcR8Q9vaBX8jJngNNT9z6egAQpa0b0foJhTe'
+});
+
+var s3 = new AWS.S3({
+    region: 'EE. UU. Este (Ohio) us-east-2'
+});
+
+
+function subirImagen() {
+    var fileInput = document.getElementById('formFileLg');
+    var file = fileInput.files[0];
+  
+    var params = {
+      Bucket: 's3-imag',
+      Key: 'nombreArchivo',
+      Body: file,
+      ACL: 'public-read'
+    };
+  
+    s3.upload(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        console.log('Imagen cargada exitosamente en S3.');
+        console.log('URL de acceso público: ' + data.Location);
+      }
+    });
+  }
+  
+
+
